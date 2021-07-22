3dcb687b7bf6f63084ef8c97bc67182005407201
shaun-mani <87504206+shaun-mani@users.noreply.github.com>
2021-07-22 16:34:40 -0400

    changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function headerInput(variable) {
+    if (variable) {
+      return "Image was clicked";
+    } else {
+      return "Image has not been clicked";
+    }
+  }
+  function imageClickHandle() {
+    return headerInput(true);
+  };
   return (
     
     <div>
       <>
+        <h2>{headerInput(false)}</h2>
+        <button onClick={imageClickHandle()}>
         <img src="/images/campus_map_half.png" alt="" />
+        </button>
     </>
     </div>
   );
