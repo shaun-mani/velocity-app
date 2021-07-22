b8570b6ca18c30cd65c965903804008bd6287187
Jaylon <jlaidlaw@uwaterloo.ca>
2021-07-22 16:57:55 -0400

    changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,78 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      deltaX: 0,
+      deltaY: 0,
+      whileMoving: false,
+      styles: {}
+    }
+
+    this.moveStart = this.moveStart.bind(this);
+    this.whileMoving = this.whileMoving.bind(this);
+    this.moveEnd = this.moveEnd.bind(this);
+
+  }
+
+  moveStart(e) {
+    this.setState ({
+      deltaX: e.screenX - e.currentTarget.getBoundingClientRect().left,
+      deltaY: e.screenY - e.currentTarget.getBoundingClientRect().top,
+      whileMoving: true
+
+    });
+  }
+
+  whileMoving(e) {
+    if(this.state.whileMoving) {
+
+      let left = e.screenX -  this.state.deltaX  ;
+      let top = e.screenY - this.state.deltaY  ;
+  
+      this.setState ({
+        styles: {
+          left: left,
+          top: top
+        }
+      });
+    }
+    
+  }
+
+  moveEnd() {
+    this.setState({
+      whileMoving: false
+    });
+  }
+   
   picClick(){
     alert("Clicked")
   }
 
-  mouseDown() {
-    alert("Mouse moved down")
+  /*dragStart() {
+    alert("You have dragged the image")
   }
+  */
+/*
+  dragEnd() {
+    alert("You have finished dragging the image")
+  }
+  */
+
 
   render() {
   return (
       <>
       <div>
-        <img onClick={this.picClick} onDragStart={this.mouseDown}
+        <h1>University of Waterloo Concept Web App</h1>
+        <img style = {this.state.styles} 
+        onClick={this.picClick} 
+        onMouseDown={this.moveStart} 
+        onMouseMove={this.whileMoving} 
+        onMouseUp = {this.moveEnd}
         src="/images/campus_map_half.png" alt="" />
         
       </div>
