26fc9efc55dcc02eb5e22ae94b7d6516db05ff63
shaun-mani <87504206+shaun-mani@users.noreply.github.com>
2021-07-29 14:31:04 -0400

    bound marker to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,21 +3,22 @@ import { useState } from "react";
 import './Map.css';
 import './image_follow.css';
 
-const getCursorPositionInElement = (evt) => {
-	return {
-		x : evt.clientX - evt.currentTarget.getBoundingClientRect().left,
-		y : evt.clientY - evt.currentTarget.getBoundingClientRect().top
-	}
-};
+// const getCursorPositionInElement = (evt) => {
+// 	return {
+// 		x : evt.clientX - evt.currentTarget.getBoundingClientRect().left,
+// 		y : evt.clientY - evt.currentTarget.getBoundingClientRect().top
+// 	}
+// };
 
 const Map = () => {
+  //const [mapPos, setMapPos] = useState({x:0, y:0});
 
   // map code
 
   const[deltaX, setDeltaX] = useState(0);
   const[deltaY, setDeltaY] = useState(0);
   const[whileMoving, setWhileMoving] = useState(false);
-  const[styles, setStyles] = useState();
+  const[styles, setStyles] = useState({left: null, top: null});
 
  const moveStart = (e) => {
     setDeltaX(e.screenX - e.currentTarget.getBoundingClientRect().left);
@@ -28,19 +29,20 @@ const Map = () => {
   const whileMoves = (e) => {
     if (whileMoving) {
         
+      //setMapPos(e.screenX - deltaX, e.screenY - deltaY)
 
       let left = e.screenX - deltaX;
       let top = e.screenY - deltaY;
 
       console.log(left, top)
      
-      if (left > -300 && top > -200 ) { 
+      //if (left > -300 && top > -200 ) { 
         setStyles({
             left: left,
             top: top
           
         });
-      }
+      //}
     }
   };
 
@@ -50,49 +52,51 @@ const Map = () => {
 
   // marker code
 
-    const [mousePos, setMousePos] = useState({x:0, y:0});
-    const [isMouseDown, setIsMouseDown] = useState(false);
+    //const [mousePos, setMousePos] = useState({x:0, y:0});
+    //const [isMouseDown, setIsMouseDown] = useState(false);
   
   
-    const handleMouseDown = () => {
-      setIsMouseDown(true);
-    }
+    //const handleMouseDown = () => {
+      //setIsMouseDown(true);
+    //}
   
-    const handleMouseUp = () => {
-      setIsMouseDown(false);
-    }
+    //const handleMouseUp = () => {
+      //setIsMouseDown(false);
+    //}
   
-    const handleMouseMove = (evt) => {
-      if (whileMoving) {
-        setMousePos(getCursorPositionInElement(evt));
-      };
-    }
+    // const handleMouseMove = (evt) => {
+    //   if (whileMoving) {
+    //     setMapPos(getCursorPositionInElement(evt));
+    //   };
+    // }
 
 
     return (
+      <div>
       <div class='Map' 
       onMouseDown= {moveStart}
       onMouseMove={whileMoves}
       onMouseUp={moveEnd}>
           <img style={styles}
           src="/images/campus_map_half.png" alt=""/>
-          
-      <div className='ImageFollow'
-			onMouseMove={handleMouseMove}
-			onMouseDown={handleMouseDown}
-			onMouseUp={handleMouseUp}
-		>
-		x:{mousePos.x} | y:{mousePos.y}
 
-		<div className='msg' style={{left: mousePos.x - 100, top: mousePos.y - 100}}>
+      <div className='ImageFollow'
+			//onMouseMove={handleMouseMove}
+			//onMouseDown={handleMouseDown}
+			//onMouseUp={handleMouseUp}
+		>
+		x:{styles.left} | y:{styles.top}
+
+		<div className='msg' style={{left: styles.left - 100, top: styles.top - 100}}>
 			marker1
 		</div>
 
-		<div className='always_half' style={{left: mousePos.x, top: mousePos.y}}>
+		<div className='always_half' style={{left: styles.left + 100, top: styles.top + 100}}>
 			marker2
 		</div>
 
 	</div>
+      </div>
       </div>
 
     );
