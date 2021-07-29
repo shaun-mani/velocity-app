9ddba589ec8f60fefa8e2019554a5fde70bc171a
Leonardo Cabrera Ortiz <leocabrera@Leonardos-MacBook-Pro.local>
2021-07-29 14:44:27 -0400

    Adding the same styling as before to new branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useState } from "react";
 import './App.css';
 import Map from './components/Map';
+import Title from './components/Title';
 
 function App() {
   return (
     <>
-    <h1>University of Waterloo Concept Web App</h1>
+    <Title />
       <Map />
     </>
   );
