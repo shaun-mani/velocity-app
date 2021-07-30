8aa06cc8e7ee28c77218d7c90e62a41f372683a1
ShiguangGu <87531781+ShiguangGu@users.noreply.github.com>
2021-07-31 00:20:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { useState } from "react";
 import './App.css';
 import Map from './components/Map';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <>
-    <h1>University of Waterloo Concept Web App</h1>
-      <Map />
+    <h1>University of Waterloo Concept Web App</h1> 
+    <React.Fragment>
+        <div><Map /></div>
+        <div><Sidebar /></div>
+        </React.Fragment>
     </>
   );
 }
