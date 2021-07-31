11d4e438b2da0462fec3e65d5a048c2afc117a82
ShiguangGu <87531781+ShiguangGu@users.noreply.github.com>
2021-07-31 22:54:04 +0800

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,11 +4,17 @@ import './Sidebar.css';
 
 
 const Sidebar = () => {
-  
+    
     return (
-      
+      <React.Fragment>
       <div class='Sidebar'/>
-      
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#map">Map</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      </React.Fragment>
     );
 
 };
