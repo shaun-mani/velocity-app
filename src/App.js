f26226ab78dd3f0ccd42e0d7dfc4808893476e6b
Leonardo Cabrera Ortiz <leocabrera@Leonardos-MacBook-Pro.local>
2021-07-31 23:47:26 -0400

    added header again, plus might have deleted a lot of node modules, but node modules are still there so idk what that is about. If something goes wrong in the future its this step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useState } from "react";
 import './App.css';
 import Map from './components/Map';
 import Sidebar from './components/Sidebar';
+import Title from './components/Title';
 
 function App() {
   return (
     <>
-    <h1>University of Waterloo Concept Web App</h1> 
+    <Title />
+    {/* <h1>University of Waterloo Concept App</h1> */}
     <React.Fragment>
     <div><Sidebar /></div>
         <div><Map /></div>
