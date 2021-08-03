f816247a2b3ece0b15a479d8d3935279e32ce81c
ShiguangGu <87531781+ShiguangGu@users.noreply.github.com>
2021-08-03 17:17:53 +0800

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useState } from "react";
 import './Sidebar.css';
 
+// **** This is just a test, did not really work out ********
 
+// changed from <li> to <nav> since the footer also uses <li> and they are aligned differently.
 const Sidebar = () => {
     
     return (
@@ -10,10 +12,11 @@ const Sidebar = () => {
       
       
       <ul className="sidebar-list">
-        <li className="sidebar-li"><a href="#home"><h3>Home</h3></a></li>
-        <li className="sidebar-li"><a href="#map"><h3>Map</h3></a></li>
-        <li className="sidebar-li"><a href="#contact"><h3>Contact</h3></a></li>
-        <li className="sidebar-li"><a href="#about"><h3>About</h3></a></li>
+      
+        <nav className="sidebar-nav"><a href="#home"><h3>Home</h3></a></nav>
+        <nav className="sidebar-nav"><a href="#map"><h3>Map</h3></a></nav>
+        <nav className="sidebar-nav"><a href="#contact"><h3>Contact</h3></a></nav>
+        <nav className="sidebar-nav"><a href="#about"><h3>About</h3></a></nav>
       </ul>
       </React.Fragment>
     );
