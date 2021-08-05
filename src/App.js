f60e457616acebb8f76ba2a35828bbead23fbbd9
shaun-mani <87504206+shaun-mani@users.noreply.github.com>
2021-08-05 16:21:19 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { useState } from "react";
 import './App.css';
 import Map from './components/Map';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
     <h1>University of Waterloo Concept Web App</h1>
-      <Map />
+       <Map />
     </>
   );
 }
