1582c9c9fc5041a409acbd5d99b1e4cf3a4d1bf5
shaun-mani <87504206+shaun-mani@users.noreply.github.com>
2021-08-09 16:51:18 -0400

    changes

--- src/components/Verticalmenu.js
+++ src/components/Verticalmenu.js
@@ -12,22 +12,22 @@ const Verticalmenu = () => {
       
       
       <div class="Verticalmenu">
-  <a href="#" class="top">Categories</a>
-  <a href="#">Funding</a>
-  <a href="#">Pitch Competitions</a>
-  <a href="#">Skills Building</a>
-  <a href="#">Coaching/Mentorship</a>
-  <a href="#">Social Justice</a>
-  <a href="#">Environmental</a>
-  <a href="#">Small Business</a>
-  <a href="#">Intrapreneurship</a>
-  <a href="#">Linked to academics</a>
-  <a href="#">Networking</a>
-  <a href="#">Research</a>
-  <a href="#">Legal</a>
-  <a href="#">Community</a>
-  <a href="#">Events</a>
-  <a href="#">Awards</a>
+  <a class="top"> Categories</a>
+  <a >Funding</a>
+  <a >Pitch Competitions</a>
+  <a >Skills Building</a>
+  <a >Coaching/Mentorship</a>
+  <a >Social Justice</a>
+  <a >Environmental</a>
+  <a >Small Business</a>
+  <a >Intrapreneurship</a>
+  <a >Linked to academics</a>
+  <a >Networking</a>
+  <a >Research</a>
+  <a >Legal</a>
+  <a >Community</a>
+  <a >Events</a>
+  <a >Awards</a>
       </div>
       </React.Fragment>
     );
