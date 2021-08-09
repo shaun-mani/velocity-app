7f9c0a0e6d3e7a43aaf95ff3ef378eda1a3972b1
leocabrera35 <leonardo.cabrera.ort@gmail.com>
2021-08-09 18:20:41 -0400

    changes to header, added picture from the concept velocity website

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Header.css';
 import Uwlogo from '../imageuwlogo.png'
+import concept_velocity_uw_img from '../concept_velocity_uw_img.png'
 
 
 function Header() {
   return (
     <div className="Title">
       <h1>
+      <img className="concept_v_uw_image" src={concept_velocity_uw_img} width="492" height="100" alt="c-v-uw-logo" />
           University of Waterloo Concept App
-        <img className="uw-image" src={Uwlogo} width="100" height="100" alt="uwlogo" />
-        <h4 className="Title-Subtitle">Presented by Velocity</h4>
+        {/* <img className="uw-image" src={Uwlogo} width="100" height="100" alt="uwlogo" /> */}
+            <h4 className="Title-Subtitle">Presented by Velocity</h4>
     </h1>
     </div>
   )
