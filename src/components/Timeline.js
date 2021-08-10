d6d9d8b8fe328b3655824769e06ae4b1f1df24b1
Jaylon <jlaidlaw@uwaterloo.ca>
2021-08-10 17:21:04 -0400

    changes

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,61 +8,6 @@ const Timeline = (props) => {
   return (
     <div>
 
-<<<<<<< HEAD
-      <div className="text-center">
-        <Button variant="outline-dark" onClick={() => {
-          props.setKeyword(null);
-          if (props.currentKeynum === 1) {
-            props.setKeynum(null);
-          } else {
-            props.setKeynum(1)
-          }
-        }}>
-          1. Getting Started
-        </Button>
-        <Button variant="outline-dark" onClick={() => {
-          props.setKeyword(null);
-          if (props.currentKeynum === 2) {
-            props.setKeynum(null);
-          } else {
-            props.setKeynum(2)
-          }
-          
-        }}>
-          2. Early Stage & problem Identification
-        </Button>
-        <Button variant="outline-dark" onClick={() => {
-          props.setKeyword(null);
-          if (props.currentKeynum === 3) {
-            props.setKeynum(null);
-          } else {
-            props.setKeynum(3)
-          }
-        }}>
-          3. Building & Testing Your Idea
-        </Button>
-        <Button variant="outline-dark" onClick={() => {
-          props.setKeyword(null);
-          if (props.currentKeynum === 4) {
-            props.setKeynum(null);
-          } else {
-            props.setKeynum(4)
-          }
-        }}>
-          4. Funding & Competitions
-        </Button>
-        <Button variant="outline-dark" onClick={() => {
-          props.setKeyword(null);
-          if (props.currentKeynum === 5) {
-            props.setKeynum(null);
-          } else {
-            props.setKeynum(5)
-          }
-        }}>
-          5. Starting Up & Launching
-        </Button>
-      </div>
-=======
       <h3 className="Timeline-title">
         What stage are you in your entrepreneurial journey?
       </h3>
@@ -121,7 +66,6 @@ const Timeline = (props) => {
           </Button>
         {/* </div> */}
       </ButtonGroup>
->>>>>>> 7f34c9920ba4bf6896a2785d69de0e3ad24f4622
     </div>
   );
 
