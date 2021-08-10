693925791e7f2c50f19201e2d8aae9675bf4a457
Jaylon <jlaidlaw@uwaterloo.ca>
2021-08-10 17:14:09 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,10 @@ function Header() {
   return (
     <div className="Title">
       <h1>
-      <img className="concept_v_uw_image" src={concept_velocity_uw_img} width="492" height="100" alt="c-v-uw-logo" />
-           Entrepreneurship EcoSystem Map
+      <img  className="concept_v_uw_image" src={concept_velocity_uw_img} width="492" height="100"  alt="c-v-uw-logo"  />
+      <h2 classname="heading">Entrepreneurship EcoSystem Map</h2>
         {/* <img className="uw-image" src={Uwlogo} width="100" height="100" alt="uwlogo" /> */}
-            <h4 className="Title-Subtitle">Concept by Velocity</h4>
+           
     </h1>
     </div>
   )
