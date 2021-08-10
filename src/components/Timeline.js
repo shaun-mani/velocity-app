ab4084e9c306e92187395e3657a46ba960483658
shaun-mani <87504206+shaun-mani@users.noreply.github.com>
2021-08-10 14:14:31 -0400

    timeline function fix

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,39 +5,65 @@ import Button from 'react-bootstrap/Button';
 
 const Timeline = (props) => {
 
-  function timelineSet(number) {
-    if (props.currentKeynum === null) {
-      props.setKeynum(number);
-      
-    } else {
-      props.setKeynum(null);
-    }
-  };
+  return (
+    <div>
 
-    return (
-      <div>
-
-        <div className="text-center">
-          <Button variant="outline-dark" onClick={timelineSet(1)}>
-            1. Getting Started
-          </Button>
-          <Button variant="outline-dark" onClick={timelineSet(2)}>
-            2. Early Stage & problem Identification
-          </Button>
-          <Button variant="outline-dark" onClick={timelineSet(3)}>
-            3. Building & Testing Your Idea
-          </Button>
-          <Button variant="outline-dark" onClick={timelineSet(4)}>
-            4. Funding & Competitions
-          </Button>
-          <Button variant="outline-dark" onClick={timelineSet(5)}>
-            5. Starting Up & Launching 
-          </Button>
-        </div>
+      <div className="text-center">
+        <Button variant="outline-dark" onClick={() => {
+          props.setKeyword(null);
+          if (props.currentKeynum === null) {
+            props.setKeynum(1);
+          } else {
+            props.setKeynum(null)
+          }
+        }}>
+          1. Getting Started
+        </Button>
+        <Button variant="outline-dark" onClick={() => {
+          props.setKeyword(null);
+          if (props.currentKeynum === null) {
+            props.setKeynum(1);
+          } else {
+            props.setKeynum(null)
+          }
+        }}>
+          2. Early Stage & problem Identification
+        </Button>
+        <Button variant="outline-dark" onClick={() => {
+          props.setKeyword(null);
+          if (props.currentKeynum === null) {
+            props.setKeynum(1);
+          } else {
+            props.setKeynum(null)
+          }
+        }}>
+          3. Building & Testing Your Idea
+        </Button>
+        <Button variant="outline-dark" onClick={() => {
+          props.setKeyword(null);
+          if (props.currentKeynum === null) {
+            props.setKeynum(1);
+          } else {
+            props.setKeynum(null)
+          }
+        }}>
+          4. Funding & Competitions
+        </Button>
+        <Button variant="outline-dark" onClick={() => {
+          props.setKeyword(null);
+          if (props.currentKeynum === null) {
+            props.setKeynum(1);
+          } else {
+            props.setKeynum(null)
+          }
+        }}>
+          5. Starting Up & Launching
+        </Button>
       </div>
-    );
+    </div>
+  );
 
 }
 
- 
+
 export default Timeline;
