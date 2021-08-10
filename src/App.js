4cacf762764812285b1524c23e676d99abd5ff7f
Mukund Rana <mk3rana@uwaterloo.ca>
2021-08-09 21:40:18 -0400

    filtering

--- src/App.js
+++ src/App.js
@@ -22,23 +22,29 @@ import {
 } from "react-google-maps";
 import * as parksData from "./data/Buildings.json";
 
-function Map() {
+function Map(props) {
   const [selectedPark, setSelectedPark] = useState(null);
+  console.log(props.currentKeyword);
   return (
     <GoogleMap
       defaultZoom={15}
       defaultCenter={{ lat: 43.469761, lng: -80.538811 }}
     >
-      {parksData.buildings.map((park) => (
-        <Marker
-          key={park.buildingId}
-          position={{ lat: park.latitude, lng: park.longitude }}
-          onClick={() => {
-            setSelectedPark(park);
-            
-          }}
-        />
-      ))}
+      {parksData.buildings
+        .filter(
+          (building) =>
+            props.currentKeyword === null ||
+            building.keyword === props.currentKeyword
+        )
+        .map((park) => (
+          <Marker
+            key={park.buildingId}
+            position={{ lat: park.latitude, lng: park.longitude }}
+            onClick={() => {
+              setSelectedPark(park);
+            }}
+          />
+        ))}
 
       {selectedPark && (
         <InfoWindow
@@ -59,10 +65,10 @@ function Map() {
   );
 }
 
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
 function App() {
+  const [currentKeyword, setKeyword] = useState(null);
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
+
   return (
     <div className="page-container">
       <div className="content-wrap">
@@ -70,28 +76,26 @@ function App() {
         <React.Fragment>
           {/* <NavBar /> */}
           {/* <MarkerClick /> */}
-          
+
           {/* <FunctionClickTest0 /> */}
           {/* <ButtonClickTest1 /> */}
           {/* <Test /> */}
           <div style={{ width: "70vw", height: "80vh" }}>
             <WrappedMap
+              currentKeyword={currentKeyword}
               googleMapURL={
                 "https://maps.googleapis.com/maps/api/js?key=AIzaSyD0LW50_GtYuB0nlw5-YhW5i1uBCGNe3XA&v=3.exp&libraries=geometry,drawing,places"
               }
               loadingElement={<div style={{ height: "100%" }} />}
               containerElement={<div style={{ height: "100%" }} />}
               mapElement={<div style={{ height: "100%" }} />}
-              
             />
           </div>
         </React.Fragment>
         <Timeline />
-        <Verticalmenu 
-        
-        />
+        <Verticalmenu setKeyword={setKeyword} currentKeyword={currentKeyword} />
       </div>
-      
+
       <Footer />
     </div>
   );
--- src/components/Verticalmenu.js
+++ src/components/Verticalmenu.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import './Verticalmenu.css';
-
+import "./Verticalmenu.css";
 
 // **** This is just a test, did not really work out ********
 
 // changed from <li> to <nav> since the footer also uses <li> and they are aligned differently.
-const Verticalmenu = () => {
-    
-    return (
-      <React.Fragment>
-      
-      
+const Verticalmenu = (props) => {
+  const words = ["Funding", "Pitch Competitions","Skills Building", "Coaching/Mentorship", "Social Justice", "Environmental", "Small Business", "Intrapreneurship", "Linked to academics", "Networking", "Research", "Legal", "Community", "Events", "Awards"];
+  return (
+    <React.Fragment>
       <div class="Verticalmenu">
-        <h4  class="top">Categories</h4>
-        <a href="#">Funding</a>
-        <a href="#">Pitch Competitions</a>
-        <a href="#">Skills Building</a>
+        <h4 class="top">Categories</h4>
+        {words.map((word) => (
+          <h1
+            class="top"
+            onClick={() => {
+              if (props.currentKeyword === word) {
+                props.setKeyword(null);
+              } else {
+                props.setKeyword(word);
+              }
+            }}
+          >
+            {word}
+          </h1>
+        ))}
+
+        {/* <h1>Pitch Competitions</h1> */}
+        {/* <a href="#">Skills Building</a>
         <a href="#">Coaching/Mentorship</a>
         <a href="#">Social Justice</a>
         <a href="#">Environmental</a>
@@ -28,11 +39,10 @@ const Verticalmenu = () => {
         <a href="#">Legal</a>
         <a href="#">Community</a>
         <a href="#">Events</a>
-        <a href="#">Awards</a>
+        <a href="#">Awards</a> */}
       </div>
-      </React.Fragment>
-    );
-
+    </React.Fragment>
+  );
 };
 
 export default Verticalmenu;
