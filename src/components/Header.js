a2ad594aa123a79f4882e1fa3e296fd2d2298fc6
Jaylon <jlaidlaw@uwaterloo.ca>
2021-08-12 14:57:23 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ import Concept_logo from '../images/concept_velocity_logo.png'
 
 function Header() {
   return (
+    <div className="Bar">
+    
+      <a className = "link" href = "https://uwaterloo.ca/coronavirus/">Visit the University of Waterloo COVID-19 information website to learn about affected campus activities. </a>
     <div className="TitleBar">
       <a href = "https://uwaterloo.ca/">
       <img  className="uw_image" src={University_logo} width="189" height="75.78"  alt="c-v-uw-logo"  />
@@ -17,6 +20,7 @@ function Header() {
         {/* <img className="uw-image" src={Uwlogo} width="100" height="100" alt="uwlogo" /> */}
       
   
+    </div>
     </div>
   )
 }
